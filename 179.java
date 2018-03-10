0db8a29a62177fe0f736e781f939ea69fb6e596a
Huiqing666 <32041618+Huiqing666@users.noreply.github.com>
2018-03-09 21:58:41 -0600

    Update 179.java

--- 179.java
+++ 179.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public String largestNumber(int[] nums) {
+        //Arrays.sort(nums);
+        int len = nums.length;
+        String[] res = new String[len];
+        for(int i = 0; i < len; i++){
+            res[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(res, (s1, s2) -> ((s2 + s1).compareTo(s1 + s2)));
+        String result = String.join("", res);
+        if (result.isEmpty() || result.charAt(0) == '0') {
+            return "0";
+        }
+        
+        return result;
+    }
+}
