2f720ce228cd2098407724bc1f0e0d3198a9ba4a
Huiqing666 <32041618+Huiqing666@users.noreply.github.com>
2018-03-10 00:33:04 -0600

    Update 005.java

--- 005.java
+++ 005.java
@@ -1,1 +1,21 @@
-
+class Solution {
+    private int lo = 0, length = 0;
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        for(int i = 0; i < len - 1; i++){
+            palindrome(s, i, i+1);
+            palindrome(s, i, i);
+        }
+        return s.substring(lo, lo + length);
+    }
+    private void palindrome(String s, int i, int j){
+        while(i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)){
+            i--;
+            j++;
+        }
+        if(length < j - i - 1){
+            lo = i + 1;
+            length = j - i - 1;
+        }
+    }
+}
