e279870cc698f7fd73b711d074d0e837b3a83848
Huiqing666 <32041618+Huiqing666@users.noreply.github.com>
2018-03-17 14:43:19 -0500

    Create 090.java

---
+++ 090.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, 0);
+        return list;
+        
+    }
+    private void backtrack(List<List<Integer>> list, List<Integer> templist, int[] nums, int start){
+        list.add(new ArrayList<>(templist));
+        for(int i = start; i < nums.length; i++){
+            if(i > start && nums[i] == nums[i-1])continue;
+            templist.add(nums[i]);
+            backtrack(list, templist, nums, i+1);
+            templist.remove(templist.size() - 1);
+
+        }
+        
+        /*
+        List temp = new ArrayList<>(templist);
+        if(!list.contains(temp)) list.add(temp);
+        for(int i = start; i < nums.length; i++){
+            templist.add(nums[i]);
+            backtrack(list, templist, nums, i + 1);
+            templist.remove(templist.size() - 1);
+
+        }
+        */
+    }
+}
