8b42fb99153db664681f5f95107858a762d26fe9
Huiqing666 <32041618+Huiqing666@users.noreply.github.com>
2018-06-01 12:56:03 +0800

    Create 144.java

---
+++ 144.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    List<Integer> res = new LinkedList<Integer>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if(root == null) return res;
+        res.add(root.val);
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+        return res;
+    }
+}
