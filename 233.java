cee0288d06bb0e8a508b636f350b2df09eb4ef8a
Huiqing666 <32041618+Huiqing666@users.noreply.github.com>
2018-07-27 15:45:42 +0800

    Create 233.java

---
+++ 233.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countDigitOne(int n) {
+        int res = 0;
+        for(long m = 1; m <=n; m *= 10) {
+            long a = n / m, b = n % m;
+            res += (a + 8)/10 * m + (a % 10 == 1 ? b + 1: 0);
+            
+            
+        }
+        return res;
+        
+    }
+}
