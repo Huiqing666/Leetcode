fbb3413b6772b73c296d3e9baa03385b3c6a9037
Huiqing666 <32041618+Huiqing666@users.noreply.github.com>
2018-09-25 23:13:06 -0500

    Create 543.java

---
+++ 543.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root == null) return 0;
+        int left = diameterOfBinaryTree(root.left);
+        int right = diameterOfBinaryTree(root.right);
+        int middle = maxDepth(root.left) + maxDepth(root.right);
+        return Math.max(Math.max(left, right), middle);
+        
+        
+        
+    }
+    private int maxDepth(TreeNode root){
+        if (root == null) return 0;
+        return Math.max(1 + maxDepth(root.left), 1 + maxDepth(root.right)); 
+    }
+}
