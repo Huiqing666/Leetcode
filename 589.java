b90eb905b947d7bf2409d77e8b156ec244b9687f
Huiqing666 <32041618+Huiqing666@users.noreply.github.com>
2018-10-10 12:28:48 -0500

    Create 589.java

---
+++ 589.java
@@ -0,0 +1,30 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> list = new ArrayList<>();
+        if(root == null) return list;
+        helper(list, root);
+        return list;
+        
+    }
+    private void helper(List<Integer> list, Node root) {
+        list.add(root.val);
+        List<Node> c = root.children;
+        for(int i = 0; i < c.size(); i++) {
+            helper(list, c.get(i));
+        }
+    }
+}
