2a96d212ec611c0fa2c86ddbe41f4a994551b9f1
Huiqing666 <32041618+Huiqing666@users.noreply.github.com>
2018-12-10 12:46:41 -0600

    Create 908.java

---
+++ 908.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int smallestRangeI(int[] A, int K) {
+        int diff = 0, result = 0, min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        
+        //find min and max in A
+        for(int i=0; i<A.length; i++) {
+            if(min > A[i]) min = A[i];
+            if(max < A[i]) max = A[i];
+        }
+        
+        diff = max - min;
+        result = diff - (K*2);
+        return (result <= 0) ? 0 : result;
+        
+    }
+}
