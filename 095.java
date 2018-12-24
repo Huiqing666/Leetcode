48f0df16d0eae6bc82113119b6289e760fa2d474
Huiqing666 <32041618+Huiqing666@users.noreply.github.com>
2018-12-24 10:34:43 -0800

    Create 095.java

---
+++ 095.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n < 1) return new ArrayList<>();
+        return generate(1, n);
+        
+    }
+    private List<TreeNode> generate(int start, int end) {
+        List<TreeNode> res = new ArrayList<>();
+        if (start > end) {
+            res.add(null);
+            return res;
+        }
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> left = generate(start, i - 1);
+            List<TreeNode> right = generate(i + 1, end);
+            for (TreeNode l : left) {
+                for (TreeNode r: right) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = l;
+                    root.right = r;
+                    res.add(root);
+                }
+            }
+        }
+        return res;
+    }
+}
