d18992f2fc75c004489d9cf22f049ca606b48e91
Huiqing666 <32041618+Huiqing666@users.noreply.github.com>
2018-12-30 22:53:29 -0800

    Create 259.java

---
+++ 259.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int threeSumSmaller(int[] nums, int target) {
+        Arrays.sort(nums);
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] + nums[j] + nums[k] < target) {
+                        count++;
+                    }
+                    else{
+                        break;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
