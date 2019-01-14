046a3827316f4fc9865796a158bb50a4af9a7a11
Huiqing666 <32041618+Huiqing666@users.noreply.github.com>
2019-01-14 10:51:48 -0600

    Create 402.java

---
+++ 402.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String removeKdigits(String num, int k) {
+        if (k >= num.length()) return "0";
+        Stack<Character> stack = new Stack<>();
+        int i = 0;
+        while (i < num.length()) {
+            while (k > 0 && !stack.isEmpty() && stack.peek() > num.charAt(i)) {
+                stack.pop();
+                k--;
+            }
+            stack.push(num.charAt(i));
+            i++;
+        }
+        while (k > 0 && !stack.empty()) {
+            stack.pop();
+            k--;
+        }
+        // System.out.println("a");
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        sb.reverse();
+        while (sb.length() > 0 && sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
+        }
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
