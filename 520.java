0e2e64830991f5947dba53d6aacbc50ed5e40573
Huiqing666 <32041618+Huiqing666@users.noreply.github.com>
2019-01-21 00:51:09 -0600

    Create 520.java

---
+++ 520.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if (word.equals(word.toUpperCase())) return true;
+        if (word.equals(word.toLowerCase())) return true;
+        if (word.substring(1).equals(word.substring(1).toLowerCase())) return true;
+        return false;
+    }
+}
