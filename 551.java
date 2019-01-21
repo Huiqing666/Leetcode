24cac043750a4a56a5d14532f2d1c17bd8fa25f2
Huiqing666 <32041618+Huiqing666@users.noreply.github.com>
2019-01-21 14:33:10 -0600

    Create 551.java

---
+++ 551.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean checkRecord(String s) {
+        int countA = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'A') countA++;
+            if (i >= 2 && s.charAt(i) == 'L' && s.charAt(i - 1) == 'L' && s.charAt(i - 2) == 'L') return false;
+        }
+        if (countA > 1) return false;
+        return true;
+    }
+}
