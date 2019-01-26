5d9ae8504f310daabb874d1ff288bda45e973204
Huiqing666 <32041618+Huiqing666@users.noreply.github.com>
2019-01-26 13:10:50 -0600

    Create 881.java

---
+++ 881.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int count = 0;
+        int i = 0, j = people.length - 1;
+        while (i < j) {
+            if (people[i] + people[j] > limit) {
+                j--;
+                count++;
+            }
+            else {
+                count++;
+                j--;
+                i++;
+            }
+        }
+        if (i == j) count++;
+        return count;
+    }
+}
