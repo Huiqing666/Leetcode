7270f38fdf587e0f4f580e2e81a94529d584c632
huiqing17 <32041618+huiqing17@users.noreply.github.com>
2019-09-27 00:15:03 -0500

    Create 305.java

---
+++ 305.java
@@ -0,0 +1,45 @@
+class Solution {
+    int[][] dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+    public List<Integer> numIslands2(int m, int n, int[][] positions) {
+        List<Integer> res = new ArrayList<>();
+        if (m <= 0 || n <= 0) return res;
+        
+        int count = 0;
+        int[] roots = new int[m * n];
+        Arrays.fill(roots, -1);
+        
+        for (int[] p : positions) {
+            int root = n * p[0] + p[1];
+            
+            if (roots[root] != -1) {    // duplicate position
+                res.add(count);
+                continue;
+            }
+            roots[root] = root; // add new island
+            count++;
+            
+            for (int[] dir : dirs) { // four directions
+                int x = p[0] + dir[0];
+                int y = p[1] + dir[1];
+                int nb = n * x + y;
+                if (x < 0 || x >= m || y < 0 || y >= n || roots[nb] == -1) continue;
+                
+                int rootNb = findIsland(roots, nb);
+                if (root != rootNb) {
+                    roots[root] = rootNb;
+                    root = rootNb;
+                    count--;
+                }
+            }
+            res.add(count);
+        }
+        return res;
+    } 
+    private int findIsland(int[] roots, int id) {
+        while (roots[id] != id) {
+            // roots[id] = findIsland(roots, roots[id]); 
+            id = roots[id];
+        }
+        return id;
+    }
+}
